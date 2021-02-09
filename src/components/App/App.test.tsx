3fc8e2c504691d3dc8f47f3f3329c0e65ce18bf4
Justin Stoddard <justinstoddard@Justins-MacBook-Pro.local>
2021-02-08 21:29:59 -0700

    added a teting package

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test("Test if a user can create a todo", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const createTodoButton = screen.findByTestId("create-todo-button");
+  console.log("Hey", createTodoButton);
 });
